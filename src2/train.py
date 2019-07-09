9c7416d5639b01dc0a63bc86177b9ddfd7a5887a
冷玄夜 <52613696+mao0617222@users.noreply.github.com>
2019-07-09 16:35:28 +0800

    阿噗赖

--- src2/train.py
+++ src2/train.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     raw_data = preprocess.readData(config)
     sms_text, sms_label = preprocess.cleanData(raw_data)
     x_train, y_train, x_val, y_val= preprocess.categorical(sms_text, sms_label)
-    embedding_matrix=preprocess.train_dic(sms_text)
-    clf = modelCNN.trainCNN(x_train, y_train, x_val, y_val, embedding_matrix)
+    embedding_layer=preprocess.train_dic(sms_text)
+    clf = modelCNN.trainCNN(x_train, y_train, x_val, y_val, embedding_layer)
     saveModel(clf, "CNN", config)
  
